65d16a39111dd3bf30c09b74728b76696b7f46f7
Walt Wengert <walt.frederick.john@live.com>
2020-11-26 14:02:14 +1000

    Removed scroll offset when setting buttons to fixed

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ document.addEventListener(
 
     //if the user has scrolled past the heading set fixed top to 0,
     //otherwise set it to the amount scrolled down the heading
-    if (window.scrollY > headingHeight + buttonsHeight / 2) {
+    if (window.scrollY > headingHeight) {
       buttons.style.position = "fixed";
       buttons.style.top = "0";
     }
